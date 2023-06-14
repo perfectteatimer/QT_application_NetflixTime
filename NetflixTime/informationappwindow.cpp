994faead136c0e3bfbf5b5090a546b80fc06ac3d
username1 <73842611+perfectteatimer@users.noreply.github.com>
2023-06-14 20:58:25 +0300

    Update informationappwindow.cpp colour changed

--- NetflixTime/informationappwindow.cpp
+++ NetflixTime/informationappwindow.cpp
@@ -9,8 +9,8 @@ informationAppWindow::informationAppWindow(QWidget *parent) :
     ui->setupUi(this);
     this->setWindowTitle("Information");
 
- 
-
+    QString styleSheet = "background-color: black;";
+    setStyleSheet(styleSheet);
     QString buttonStyleSheet = "QPushButton { background-color: olive; }";
     ui->backToMainwindow->setStyleSheet(buttonStyleSheet);
 }
